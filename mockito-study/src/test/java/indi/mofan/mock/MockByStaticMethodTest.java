7f42e76b8094936c152e04a4dc7a230314553db4
mofan <cy.mofan@foxmail.com>
2023-11-09 22:26:04 +0800

    使用静态方法 Mock 对象

--- mockito-study/src/test/java/indi/mofan/mock/MockByStaticMethodTest.java
+++ mockito-study/src/test/java/indi/mofan/mock/MockByStaticMethodTest.java
@@ -3,15 +3,12 @@ package indi.mofan.mock;
 import indi.mofan.entity.Account;
 import indi.mofan.helloworld.dao.AccountDao;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * @author mofan 2020/12/18
  */
-@ExtendWith(MockitoExtension.class)
-public class MockByRunnerTest {
+public class MockByStaticMethodTest {
     @Test
     public void testMock() {
         AccountDao accountDao = Mockito.mock(AccountDao.class, Mockito.RETURNS_SMART_NULLS);
